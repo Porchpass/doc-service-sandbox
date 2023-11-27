417abf382465ec5e129be8e8188fb346c91a962f
8ctopotamus <zylo.codes@gmail.com>
2023-11-27 15:10:48 -0600

    jinja test

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-import os
-from dotenv import load_dotenv
-import docraptor
+############
+# Jinja test
 
-load_dotenv()
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-docraptor_api_key = os.getenv('DOCRAPTOR_API_KEY')
-doc_api = docraptor.DocApi()
-doc_api.api_client.configuration.username = docraptor_api_key
+template_loader = FileSystemLoader('templates')
+env = Environment(
+  loader=template_loader,
+  autoescape=select_autoescape()
+)
 
-response = doc_api.create_doc({
-  "test": True,
-  "document_content": "<html><body>Hello World</body></html>",
-  "name": "docraptor-sandbox.pdf",
-  "document_type": "pdf",
-})
+template = env.get_template('index.html.jinja')
+html = template.render(name="Jinja2")
 
-# ENV test
+print(html)
+
+################
+# DocRaptor test
+
+# import os
+# from dotenv import load_dotenv
+# import docraptor
+
+# load_dotenv()
+
+# docraptor_api_key = os.getenv('DOCRAPTOR_API_KEY')
+# doc_api = docraptor.DocApi()
+# doc_api.api_client.configuration.username = docraptor_api_key
+
+# response = doc_api.create_doc({
+#   "test": True,
+#   "document_content": "<html><body>Hello World</body></html>",
+#   "name": "docraptor-sandbox.pdf",
+#   "document_type": "pdf",
+# })
