3d26b7a006718bb05e3d1f2c093a60b2db6a87ba
8ctopotamus <zylo.codes@gmail.com>
2023-11-27 15:58:36 -0600

    write temp.html file for previewing in browser

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from utils import docraptor, templating
+from src.utils import docraptor, templating
 
 # render an html template
 html = templating.render_template()
 print(html)
+
+# write file (to preview in browser)
+f = open('temp.html', 'w')
+f.write(html)
+f.close()
 
 # create pdf in DocRaptor
 # response = docraptor.create_doc()
--- src/utils/templating.py
+++ src/utils/templating.py
@@ -1,6 +1,6 @@
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
-template_loader = FileSystemLoader('templates')
+template_loader = FileSystemLoader('src/templates')
 
 env = Environment(
   loader=template_loader,
