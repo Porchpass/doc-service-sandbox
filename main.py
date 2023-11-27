b04b5068fe6c8df683be0a8cb2b4c7c42a6c6aa7
8ctopotamus <zylo.codes@gmail.com>
2023-11-27 14:41:16 -0600

    create_doc test

--- main.py
+++ main.py
@@ -5,17 +5,14 @@ import docraptor
 load_dotenv()
 
 docraptor_api_key = os.getenv('DOCRAPTOR_API_KEY')
-print(docraptor_api_key)
+doc_api = docraptor.DocApi()
+doc_api.api_client.configuration.username = docraptor_api_key
 
-
-# doc_api = docraptor.DocApi()
-# doc_api.api_client.configuration.username = docraptor_api_key
-
-# response = doc_api.create_doc({
-#   "test": True,
-#   "document_content": "<html><body>Hello World</body></html>",
-#   "name": "docraptor-sandbox.pdf",
-#   "document_type": "pdf",
-# })
+response = doc_api.create_doc({
+  "test": True,
+  "document_content": "<html><body>Hello World</body></html>",
+  "name": "docraptor-sandbox.pdf",
+  "document_type": "pdf",
+})
 
 # ENV test
