e1d5e339a0e6bf69aac5f4425698b01a34406f47
8ctopotamus <zylo.codes@gmail.com>
2023-12-28 08:57:47 -0600

    DocServiceArgs - format according to LSP diagnostics

--- src/models/doc.py
+++ src/models/doc.py
@@ -2,7 +2,8 @@ from typing import Optional
 from pydantic import BaseModel
 
 class DocServiceArgs(BaseModel):
-  slug: str
-  use_docraptor: Optional[bool] = True
-  test_mode: Optional[bool] = False
-  # TODO: variables: dict
+
+    slug: str
+    use_docraptor: Optional[bool] = True
+    test_mode: Optional[bool] = False
+    # TODO: variables: dict
